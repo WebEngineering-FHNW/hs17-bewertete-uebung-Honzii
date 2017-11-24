966d5db2e3bde21196fabb122e0c943648e3f2f5
David Hunziker <david.hunziker3@students.fhnw.ch>
2017-11-24 22:42:45 +0100

    Deleted some unneeded stuff

--- grails-app/controllers/webec_redditclone/CreatePostController.groovy
+++ grails-app/controllers/webec_redditclone/CreatePostController.groovy
@@ -1,23 +1,9 @@
 package webec_redditclone
 
-import com.redditclone.Posts
-import com.redditclone.auth.User
 import grails.plugin.springsecurity.annotation.Secured
 
 @Secured(['ROLE_ADMIN', 'ROLE_USER'])
 class CreatePostController {
-
-    def springSecurityService
-
-    def newPost() {
-        def post = new Posts()
-        post.setUrl(params.url)
-        post.setTitel(params.title)
-        post.setAuthor(lookupPerson())
-        post.save()
-    }
-
-    private lookupPerson(){
-        User.get(springSecurityService.currentUserId)
-    }
+    def newPost() { }
+    
 }
